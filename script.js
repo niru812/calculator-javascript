e896918305889914ac08b53169dbff02e8c722c1
niru812 <91334055+niru812@users.noreply.github.com>
2022-07-10 00:15:39 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,63 @@
+const result = document.getElementById('result')
+const input = document.getElementById('input')
+const symbol = document.getElementsByClassName('symbols')
+const number = document.getElementsByClassName('numbers')
+var first = 0;
+var second = 0;
+
+Array.from(number).forEach(num =>{
+    num.addEventListener("click",()=>{
+        if(parseFloat(input.innerText)>99999999999999999999999999999999999999999|| 
+            parseFloat(result.innerText)>99999999999999999999999999999999999999999)
+            clr();
+        if(num.innerText==="." && result.innerText.includes('.')) return;
+        if(first === 0 && input.innerText !== ""){
+            input.innerHTML += num.innerText;
+            result.innerHTML += num.innerText;
+        }
+        else{
+            result.innerHTML += num.innerText;
+        }
+
+    })
+});
+
+Array.from(symbol).forEach(sym =>{
+    sym.addEventListener("click",op = ()=>{
+        if(first === 0){
+            first = parseFloat(result.innerText);
+            input.innerText = result.innerText + sym.innerText;
+            result.innerText = "";
+            operator = sym.innerText;
+        }
+        else{
+            second = parseFloat(result.innerText);
+            first = operate(first,second,operator);
+            input.innerText = first + sym.innerText;
+            result.innerText = "";
+            operator = sym.innerText;
+        }
+    })
+});
+
+document.getElementById('AC').addEventListener("click",clr= ()=>{
+    input.innerText="";
+    result.innerText="";
+    first = 0;
+})
+
+document.getElementById('eq').addEventListener("click",() =>{
+    op();
+    input.innerText = "";
+    result.innerText = first;
+    first = 0;
+})
+
+operate = (first ,second, operator) =>{
+   return operator === "+" ? first + second:
+          operator === "-" ? first - second:
+          operator === "×" ? first * second:
+          operator === "÷" ? first/second:
+          operator === "%" ? first%second:
+          null; 
+}
